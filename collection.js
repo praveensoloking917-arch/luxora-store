ae2abe0c49cc177c396d178202277c0a47bef92b
PRAVEEN KUMAR.R <praveensoloking917@gmail.com>
2026-07-15 14:12:00 +0530

    Add files via upload

---
+++ collection.js
@@ -0,0 +1,20 @@
+var productContainer=document.getElementById("products")
+var search=document.getElementById("search")
+var productlist=productContainer.querySelectorAll("div")
+
+search.addEventListener("keyup",function()
+{
+    var enteredvalue=event.target.value.toUpperCase()
+    for(i=0;i<productlist.length;i=i+1)
+    {
+        var productname=productlist[i].querySelector("p").textContent
+        if(productname.toUpperCase().indexOf(enteredvalue)<0)
+        {
+            productlist[i].style.display="none"
+        }
+        else{
+             productlist[i].style.display="block"
+        }
+    }
+
+})
---
+++ sucess.js
@@ -0,0 +1,5 @@
+document.getElementById("myform").addEventListener("submit", function(event) {
+    event.preventDefault();
+    // Handle form submission logic here
+    window.location.href = "sucess.html"; // Redirect to the success page
+});
